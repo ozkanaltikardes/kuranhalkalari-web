93e7f7de52bed00070908678dae99656c939c492
ozkanaltikardes <ozkan@biges365.com>
2025-11-24 01:13:31 +0300

    Ana sayfa yonlendirmesi eklendi

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Yönlendirme Ayarı
+  async redirects() {
+    return [
+      {
+        source: '/',      // Kaynak: Ana sayfa
+        destination: '/tr', // Hedef: Türkçe ana sayfa
+        permanent: true,    // Bu kalıcı bir yönlendirmedir (308)
+      },
+    ];
+  },
 };
 
 export default nextConfig;
